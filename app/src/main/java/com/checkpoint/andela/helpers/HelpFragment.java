f5caabf9f81bb9d9c4501e01e8a52a211b28efa9
andela-shassan <hassan.ayomon@andela.com>
2016-04-27 14:55:26 +0100

    Update the help fragment to enhance readability"
    "

--- app/src/main/java/com/checkpoint/andela/helpers/HelpFragment.java
+++ app/src/main/java/com/checkpoint/andela/helpers/HelpFragment.java
@@ -1,12 +1,17 @@
 package com.checkpoint.andela.helpers;
 
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -48,5 +53,19 @@ public class HelpFragment extends DialogFragment {
         helpText6.setText(R.string.help6);
         helpText7.setText(R.string.help7);
         getDialog().setTitle("Help?");
+    }
+
+    @Override
+    public void onResume() {
+        // Store access variables for window and blank point
+        Window window = getDialog().getWindow();
+        Point size = new Point();
+        // Store dimensions of the screen in `size`
+        Display display = window.getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        // Set the width of the dialog proportional to 75% of the screen width
+        window.setLayout((int) (size.x * 1.0), WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        super.onResume();
     }
 }
